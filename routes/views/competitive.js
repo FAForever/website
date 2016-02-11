9da26c6991ef72d1430787f188603d771c47ba50
Blodir <pyry.makinen@helsinki.fi>
2016-02-11 18:18:21 +0200

    basic challonge api functionality

--- routes/views/competitive.js
+++ routes/views/competitive.js
@@ -1,4 +1,6 @@
 var keystone = require('keystone');
+var request = require('request');
+var challonge_config = require('../challonge_config');
 
 exports = module.exports = function(req, res) {
 
@@ -9,8 +11,24 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'competitive';
   locals.cSection = 'competitive';
+	locals.data = {
+	};
+	locals.error = false;
+	locals.data.upcomingTournaments = [];
 
-	// Render the view
-	view.render('competitive');
-
+	request(challonge_config.getAuth() + '/tournaments.json', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			//console.log(body);
+			var tournaments = JSON.parse(body);
+			for (tournament of tournaments) {
+				if (tournament.tournament.state === 'pending') {
+					locals.data.upcomingTournaments.push(tournament.tournament);
+				}
+			}
+		} else {
+			locals.error = true;
+		}
+		// Render the view
+		view.render('competitive');
+	});
 };
