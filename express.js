72b3f4ff5e74ce86be271bd4eccf1f18c2fe405b
Aaron Valandra <avaland@woopta.com>
2016-07-05 11:20:05 -0500

    Added the 500 error handler if in development mode.

--- express.js
+++ express.js
@@ -33,6 +33,16 @@ app.use(function(req, res, next) {
 	res.status(404).render('errors/404');
 });
 
+//Display 500 Error Handler if in development mode.
+if (app.get('env') == 'development') {
+	app.enable('verbose errors');
+	
+	//500 Error Handler
+	app.use(function (err, req, res, next) {
+		res.status(500).render('errors/500', {error: err});
+	});
+}
+
 //Start and listen on port
 app.listen(app.get('port'), function () {
 	console.log('Express listening on port ' + app.get('port'));
