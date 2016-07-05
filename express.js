48b77ab66d9be76d8ec7c087b1185ab613d06798
Aaron Valandra <avaland@woopta.com>
2016-07-05 10:38:56 -0500

    Added comments and middleware to routing.

--- express.js
+++ express.js
@@ -4,69 +4,31 @@ require('dotenv').load();
 
 var express = require('express');
 
+var middleware = require('./routes/middleware');
+
 var app = express();
+
+//Execute middleware before each request...
+app.use('/', middleware);
+
+//Set static public directory path
 app.use(express.static('public'));
 
+//Initialize values for default configs
 app.set('views', 'templates/views');
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT);
 
-app.get('/', function(req, res) { res.render('index'); });
-app.get('/contribution', function(req, res) { res.render('contribution'); });
+//Variable to hold routing path
+var routes = './routes/views/';
 
-app.get('/competitive/tournaments', function(req, res) {
+//Define routes
+app.get('/', require(routes + 'index'));
+app.get('/contribution', require(routes + 'contribution'));
+app.get('/competitive/tournaments', require(routes + 'tournaments'));
+app.get('/competitive/leaderboards', require(routes + 'leaderboards'));
 
-	var challonge_config = require('./routes/challonge_config.js');
-	var request = require('request');
-
-	res.locals.section = 'competitive';
-	res.locals.cSection = 'tournaments';
-	res.locals.cNavLinks = [
-		{ label: 'Tournaments',		key: 'tournaments',		href: '/competitive/tournaments' },
-		{ label: 'Leaderboards',		key: 'leaderboards',		href: '/competitive/leaderboards' }
-	];
-
-	request(challonge_config.getAuth() + '/tournaments.json', function (error, response, body) {
-		var data = {};
-		var error = false;
-		data.upcomingTournaments = [];
-		data.runningTournaments = [];
-
-		if (!error && response.statusCode == 200) {
-			var tournaments = JSON.parse(body);
-			for (tournament of tournaments) {
-				if (tournament.tournament.state === 'pending') {
-					var startDate = new Date(tournament.tournament.start_at);
-					tournament.tournament.start_at = startDate.toUTCString();
-					data.upcomingTournaments.push(tournament.tournament);
-				} else if (tournament.tournament.state === 'underway' || tournament.tournament.state === 'in_progress') {
-					data.runningTournaments.push(tournament.tournament);
-				}
-			}
-		} else {
-			error = true;
-		}
-		// Render the view
-		res.render('tournaments', {error: error, data: data});
-	});
-
-});
-
-app.get('/competitive/leaderboards', function(req, res) {
-
-	res.locals.cNavLinks = [
-		{ label: 'Tournaments',		key: 'tournaments',		href: '/competitive/tournaments' },
-		{ label: 'Leaderboards',		key: 'leaderboards',		href: '/competitive/leaderboards' }
-	];
-
-	res.locals.section = 'competitive';
-	res.locals.cSection = 'leaderboards';
-
-	res.render('leaderboards');
-});
-
-
-
+//Start and listen on port
 app.listen(app.get('port'), function () {
 	console.log('Express listening on port ' + app.get('port'));
 });
