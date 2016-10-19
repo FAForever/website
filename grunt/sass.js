7991e8f9fb5353e275ba9287bdc7a01d1cafc114
Aaron Valandra <avaland@woopta.com>
2016-10-18 20:06:50 -0500

    Added accounts css.

--- grunt/sass.js
+++ grunt/sass.js
@@ -7,6 +7,7 @@ module.exports = {
         files: {
             'public/styles/css/site.min.css': 'public/styles/site.scss',
             'public/styles/css/contributions.min.css': 'public/styles/site/contributions.scss',
+            'public/styles/css/account.min.css': 'public/styles/site/account.scss',
             'public/styles/css/index.min.css': 'public/styles/site/index.scss',
             'public/styles/css/news.min.css': 'public/styles/site/news.scss',
             'public/styles/css/calendar.min.css': 'public/styles/site/calendar.scss'
@@ -20,6 +21,7 @@ module.exports = {
         files: {
             'public/styles/css/site.min.css': 'public/styles/site.scss',
             'public/styles/css/contributions.min.css': 'public/styles/site/contributions.scss',
+            'public/styles/css/account.min.css': 'public/styles/site/account.scss',
             'public/styles/css/index.min.css': 'public/styles/site/index.scss',
             'public/styles/css/news.min.css': 'public/styles/site/news.scss',
             'public/styles/css/calendar.min.css': 'public/styles/site/calendar.scss'
