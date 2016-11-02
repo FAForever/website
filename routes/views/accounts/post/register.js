d75f2d8cd2741af3a08368cceab22bb98a5ca742
Aaron Valandra <avaland@woopta.com>
2016-11-01 22:37:48 -0500

    Updated register view an implementation

--- routes/views/accounts/post/register.js
+++ routes/views/accounts/post/register.js
@@ -1,7 +1,12 @@
 var flash = {};
 var request = require('request');
+var SHA256 = require("crypto-js/sha256");
 
 exports = module.exports = function(req, res) {
+
+	var locals = res.locals;
+
+	locals.formData = req.body || {};
 
 	// validate the input
 	req.checkBody('username', 'Username is required').notEmpty();
@@ -13,39 +18,60 @@ exports = module.exports = function(req, res) {
 	// check the validation object for errors
 	var errors = req.validationErrors();
 
+	//Must have client side errors to fix
 	if (errors) {
 
 		// failure
-
-		console.log(errors);
-
 		flash.type = 'alert-danger';
-		flash.messages = [{msg: errors}];
+		flash.messages = errors;
 
-		res.render('register', {flash: flash});
+		res.render('account/register', {flash: flash});
 
 	} else {
 
 		// pull the form variables off the request body
 		var username = req.body.username;
-		var password = req.body.password;
 		var email 	 = req.body.email;
 
+		//Encrypt password before sending it off to endpoint
+		var password = SHA256(req.body.password).toString();
+
+		var overallRes = res;
+
+		//Run post to register endpoint
 		request.post({
-			url: 'api.faforever.com/users/register',
+			url: process.env.API_URL + '/users/register',
 			form : {name: username, email: email, pw_hash: password}
 		}, function (err, res, body) {
-			console.log(body);
+			//Check to see if valid user
+			if(body != 'ok') {
+				var errorMessages = [];
+
+				//Must not be valid, check to see if errors, otherwise return generic error.
+				try {
+					var errors = JSON.parse(body);
+
+					for(var i = 0; i < errors.errors.length; i++) {
+						var error = errors.errors[i];
+
+						errorMessages.push({msg: error.detail});
+					}
+				} catch(e) {
+					errorMessages.push({msg: 'Invalid registration sign up. Please try again later.'})
+				}
+
+				flash.type = 'alert-danger';
+				flash.messages = errorMessages;
+
+				overallRes.render('account/register', {flash: flash});
+			} else {
+				// Successfully registered user
+				flash.type = 'alert-success';
+				flash.messages = [{msg: 'Please check your email to verify your registration. Then you will be ready to log in!'}];
+
+				overallRes.render('account/register', {flash: flash});
+			}
 		});
-
-		// success
-
-		console.log('success');
-
-		flash.type = 'alert-success';
-		flash.messages = [{msg: 'Please check your email to verify your registration. Then you will be ready to log in!'}];
-
-		res.render('login', {flash: flash});
 
 	}
 
