09a37e44b70c59852b40304cf3a5cf8e46520652
Aaron Valandra <avaland@woopta.com>
2016-11-15 21:31:10 -0600

    Updated link to steam page to include new route.

--- routes/views/accounts/get/linkSteam.js
+++ routes/views/accounts/get/linkSteam.js
@@ -6,6 +6,10 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'linkSteam';
 
+	locals.formData = req.body || {};
+
+	locals.steam = process.env.API_URL + '/users/link_to_steam';
+
 	// Render the view
 	res.render('account/linkSteam');
 
