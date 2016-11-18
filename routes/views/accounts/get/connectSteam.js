5ca6dd628b93da3e8cd9c37429c0c1af5fccd93c
Aaron Valandra <avaland@woopta.com>
2016-11-17 19:45:40 -0600

    Added steam link/connect.

---
+++ routes/views/accounts/get/connectSteam.js
@@ -0,0 +1,38 @@
+var request = require('request');
+var flash = {};
+
+exports = module.exports = function(req, res) {
+
+	var overallRes = res;
+
+	request.post({
+		'url': process.env.API_URL + '/users/link_to_steam',
+		'headers': {'Authorization': 'Bearer ' + req.user.data.attributes.token},
+		form : {redirect_to: req.protocol + '://' + req.get('host') + '/account/link'}
+	}, function (err, res, body) {
+		//Must not be valid, check to see if errors, otherwise return generic error.
+		try {
+			body = JSON.parse(body);
+
+			if (body.steam_url) {
+				overallRes.redirect(body.steam_url);
+			}
+
+			flash.class = 'alert-danger';
+			flash.messages = [{msg: 'Your steam account was not successfully linked! Please verify you logged into the website correctly.'}];
+			flash.type = 'Error!';
+
+			overallRes.render('account/linkSteam', {flash: flash});
+
+		} catch(e) {
+			flash.class = 'alert-danger';
+			flash.messages = [{msg: 'Your steam account was not successfully linked! Please verify you logged into the website correctly.'}];
+			flash.type = 'Error!';
+
+			overallRes.render('account/linkSteam', {flash: flash});
+		}
+
+	});
+
+
+};
--- routes/views/accounts/get/linkSteam.js
+++ routes/views/accounts/get/linkSteam.js
@@ -1,3 +1,5 @@
+var flash = {};
+
 exports = module.exports = function(req, res) {
 
 	var locals = res.locals;
@@ -8,9 +10,27 @@ exports = module.exports = function(req, res) {
 
 	locals.formData = req.body || {};
 
-	locals.steam = process.env.API_URL + '/users/link_to_steam';
+	var result;
+
+	if (req.query.steam_link_result) {
+		result = req.query.steam_link_result;
+		if (result == 'success') {
+			flash.class = 'alert-success';
+			flash.messages = [{msg: 'Your steam account was successfully linked!'}];
+			flash.type = 'Success!';
+		} else {
+			flash.class = 'alert-danger';
+			flash.messages = [{msg: 'Your steam account was not successfully linked! Please verify you logged in correctly to steam.'}];
+			flash.type = 'Error!';
+		}
+	} else {
+		flash = null;
+	}
+
+	//locals.steam = process.env.API_URL + '/users/link_to_steam';
+	locals.steamConnect = req.protocol + '://' + req.get('host') + '/account/connect';
 
 	// Render the view
-	res.render('account/linkSteam');
+	res.render('account/linkSteam', {flash: flash});
 
 };
