1828ad1f2fcc02d3cf5e11b9f7138b527b557d03
lextoc <alexander.claes10@gmail.com>
2017-01-04 21:45:00 +0100

    Clean up (Part I)

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,36 +1,20 @@
 module.exports = {
-	dev: {
-        options: {
-            style: 'expanded',
-            compass: true
-        },
-        files: {
-            'public/styles/css/site.min.css': 'public/styles/site.scss',
-            'public/styles/css/contributions.min.css': 'public/styles/site/contributions.scss',
-            'public/styles/css/account.min.css': 'public/styles/site/account.scss',
-            'public/styles/css/index.min.css': 'public/styles/site/index.scss',
-            'public/styles/css/post.min.css': 'public/styles/site/post.scss',
-            'public/styles/css/news.min.css': 'public/styles/site/news.scss',
-            'public/styles/css/calendar.min.css': 'public/styles/site/calendar.scss',
-            'public/styles/css/client.min.css': 'public/styles/site/client.scss',
-            'public/styles/css/leaderboards.min.css': 'public/styles/site/leaderboards.scss'
-        }
+  dev: {
+    options: {
+      style: 'expanded',
+      compass: true
     },
-    dist: {
-        options: {
-            style: 'compressed',
-            compass: true
-        },
-        files: {
-            'public/styles/css/site.min.css': 'public/styles/site.scss',
-            'public/styles/css/contributions.min.css': 'public/styles/site/contributions.scss',
-            'public/styles/css/account.min.css': 'public/styles/site/account.scss',
-            'public/styles/css/index.min.css': 'public/styles/site/index.scss',
-            'public/styles/css/post.min.css': 'public/styles/site/post.scss',
-            'public/styles/css/news.min.css': 'public/styles/site/news.scss',
-            'public/styles/css/calendar.min.css': 'public/styles/site/calendar.scss',
-            'public/styles/css/client.min.css': 'public/styles/site/client.scss',
-            'public/styles/css/leaderboards.min.css': 'public/styles/site/leaderboards.scss'
-        }
+    files: {
+      'public/styles/css/site.min.css': 'public/styles/site.sass',
     }
+  },
+  dist: {
+    options: {
+      style: 'compressed',
+      compass: true
+    },
+    files: {
+      'public/styles/css/site.min.css': 'public/styles/site.sass',
+    }
+  }
 };
--- public/js/app/calendar.js
+++ public/js/app/calendar.js
@@ -1,49 +1,42 @@
 $(document).ready(function() {
-
-    $('.calendar').fullCalendar({
-
-        googleCalendarApiKey: calendar_key,
-        locale: locale,
-
-        eventSources: [
-            {
-                events: tournamentEvents,
-                className: 'tournamentCalendar'
-            },
-            {
-                googleCalendarId: 'faforever.com_cpellm2p2sdfr8dfmnmgs1davk@group.calendar.google.com',
-                className: 'developerCalendar'
-            }
-        ],
-
-        eventClick: function(event) {
-            // opens events in a popup window
-            window.open(event.url, 'gcalevent', 'width=700,height=600');
-            return false;
+  $('.calendar').fullCalendar({
+    googleCalendarApiKey: calendar_key,
+    locale: locale,
+    eventSources: [
+        {
+            events: tournamentEvents,
+            className: 'tournamentCalendar'
         },
-
-        loading: function(bool) {
-            $('#loading').toggle(bool);
+        {
+            googleCalendarId: 'faforever.com_cpellm2p2sdfr8dfmnmgs1davk@group.calendar.google.com',
+            className: 'developerCalendar'
         }
+    ],
+    eventClick: function(event) {
+        // opens events in a popup window
+        window.open(event.url, 'gcalevent', 'width=700,height=600');
+        return false;
+    },
+    loading: function(bool) {
+        $('#loading').toggle(bool);
+    }
+  });
 
-    });
+  $(".toggleSwitch").bootstrapSwitch();
 
-    $(".toggleSwitch").bootstrapSwitch();
+  $(".toggleDeveloper").on('switchChange.bootstrapSwitch', function(event, state) {
+    if (state) {
+      $(".calendar .developerCalendar").show();
+    } else {
+      $(".calendar .developerCalendar").hide();
+    }
+  });
 
-    $(".toggleDeveloper").on('switchChange.bootstrapSwitch', function(event, state) {
-        if (state) {
-            $(".calendar .developerCalendar").show();
-        } else {
-            $(".calendar .developerCalendar").hide();
-        }
-    });
-
-    $(".toggleTournament").on('switchChange.bootstrapSwitch', function(event, state) {
-        if (state) {
-            $(".calendar .tournamentCalendar").show();
-        } else {
-            $(".calendar .tournamentCalendar").hide();
-        }
-    });
-
+  $(".toggleTournament").on('switchChange.bootstrapSwitch', function(event, state) {
+    if (state) {
+      $(".calendar .tournamentCalendar").show();
+    } else {
+      $(".calendar .tournamentCalendar").hide();
+    }
+  });
 });
