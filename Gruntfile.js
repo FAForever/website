825d8e4baf58c0f2b14d87366a1c8b4c5d4e799f
lextoc <alexander.claes10@gmail.com>
2017-01-14 11:27:55 +0100

    Install autoprefixer (might need more config)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,53 @@
 
 module.exports = function(grunt) {
 
-	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
 
-	// Time how long tasks take. Can help when optimizing build times
-	require('time-grunt')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-	var options = {
-		config: {
-			src: './grunt/*.js'
-		},
-		pkg: grunt.file.readJSON('package.json')
-	};
+  var options = {
+    config: {
+      src: './grunt/*.js'
+    },
+    paths: {
+      public: './public'
+    },
+    pkg: grunt.file.readJSON('package.json')
+  };
 
-	var configs = require('load-grunt-configs')(grunt, options);
+  var configs = require('load-grunt-configs')(grunt, options);
 	
-	// Project configuration.
-	grunt.initConfig(configs);
+  // Project configuration.
+  grunt.initConfig(configs);
 
-	// load jshint
-	grunt.registerTask('lint', [
-		'jshint'
-	]);
+  // load jshint
+  grunt.registerTask('lint', [
+    'jshint'
+  ]);
 
-	// default option to connect server
-	grunt.registerTask('serve', [
-		'jshint',
-		'concurrent:dev'
-	]);
+  // default option to connect server
+  grunt.registerTask('serve', [
+    'jshint',
+    'concurrent:dev'
+  ]);
 
-	grunt.registerTask('prod', [
-		'sass:dist',
-		'concat:js',
-		'uglify:dist'
-	]);
+  grunt.registerTask('prod', [
+    'sass:dist',
+    'concat:js',
+    'uglify:dist'
+  ]);
 
-	grunt.registerTask('server', function () {
-		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-		grunt.task.run(['serve:' + target]);
-	});
+  grunt.registerTask('server', function () {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve:' + target]);
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-postcss');
 };
