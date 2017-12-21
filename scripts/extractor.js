24608cc5971a0dd3bdfb82c4664f144f442cf2d7
Aaron Valandra <avaland@woopta.com>
2017-12-21 17:37:40 -0600

    Added top five players to homepage and updated homepage.

--- scripts/extractor.js
+++ scripts/extractor.js
@@ -34,12 +34,12 @@ module.exports.run = function run() {
 		}
 	});
 
-	request(process.env.API_URL + '/leaderboards/1v1?filter[is_active]=true', function (error, response, body) {
+	request(process.env.API_URL + '/leaderboards/ladder1v1?filter[is_active]=true', function (error, response, body) {
 		if (! error) {
 			var users = JSON.parse(body);
 
 			var csvArray = [];
-
+        
 			for(var i = 0; i < users.data.length; i++) {
 				var user = users.data[i];
 
@@ -58,7 +58,31 @@ module.exports.run = function run() {
 					console.log(date + ' - User file created successfully for 1v1.');
 				}
 			});
+        
+        processTopFivePlayers(users);
+    }
+});
 
+function processTopFivePlayers(users)
+{
+	var topFive = [];
+
+	for(var i = 0; i < 5; i++) {
+		var user = users.data[i];
+
+		var data = {
+			name: user.attributes.name,
+			rank: user.attributes.mean
+		};
+
+		topFive.push(data);
+	}
+
+	fs.writeFile("members/top5.json", JSON.stringify(topFive), function(error) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(date + ' - User file created successfully for top five.');
 		}
 	});
 }
