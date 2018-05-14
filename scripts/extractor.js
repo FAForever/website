33880559001b561284e0204bad52eb3b511b0a52
Michel Jung <michel.jung89@gmail.com>
2018-05-14 18:35:25 +0200

    Fix syntax error

--- scripts/extractor.js
+++ scripts/extractor.js
@@ -35,36 +35,36 @@ module.exports.run = function run() {
 	});
 
 	request(process.env.API_URL + '/leaderboards/ladder1v1?filter[is_active]=true', function (error, response, body) {
-		if (! error) {
+		if (!error) {
 			var users = JSON.parse(body);
 
 			var csvArray = [];
-        
-			for(var i = 0; i < users.data.length; i++) {
+
+			for (var i = 0; i < users.data.length; i++) {
 				var user = users.data[i];
 
 				var data = {
 					label: user.attributes.login,
-					value: { id: user.id, page: Math.ceil((i+1) / 100) }
+					value: {id: user.id, page: Math.ceil((i + 1) / 100)}
 				};
 
 				csvArray.push(data);
 			}
 
-			fs.writeFile("members/1v1.json", JSON.stringify(csvArray), function(error) {
+			fs.writeFile("members/1v1.json", JSON.stringify(csvArray), function (error) {
 				if (error) {
 					console.log(error);
 				} else {
 					console.log(date + ' - User file created successfully for 1v1.');
 				}
 			});
-        
-        processTopFivePlayers(users);
-    }
-});
 
-function processTopFivePlayers(users)
-{
+			processTopFivePlayers(users);
+		}
+	});
+}
+
+function processTopFivePlayers(users) {
 	var topFive = [];
 
 	for(var i = 0; i < 5; i++) {
