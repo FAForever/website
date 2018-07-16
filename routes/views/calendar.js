d2cc82411fb53f70ef269f374988aeb3ee2eaa51
Sheeo <sheeo@sheeo.dk>
2018-07-16 10:39:37 +0200

    Refactor calendar route

--- routes/views/calendar.js
+++ routes/views/calendar.js
@@ -1,49 +1,44 @@
-var locale = require("accept-language-parser");
-var challonge_config = require('../challonge_config');
-var request = require('request');
+const locale = require("accept-language-parser");
+const challonge_config = require('../challonge_config');
+const request = require('request');
 
 exports = module.exports = function(req, res) {
+  let locals = res.locals;
 
-	var locals = res.locals;
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'calendar';
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'calendar';
+  locals.calendar = process.env.GOOGLE_CALENDAR_API_KEY;
 
-	locals.calendar = process.env.GOOGLE_CALENDAR_API_KEY;
+  let languages = locale.parse(req.headers["accept-language"]);
 
-	var languages = locale.parse(req.headers["accept-language"]);
+  if (languages.length > 0) {
+    locals.locale = languages[0].code;
+  } else {
+    locals.locale = 'en';
+  }
 
-	if (languages.length != 0) {
-		locals.locale = languages[0].code;
-	} else {
-		locals.locale = 'en';
-	}
+  request(challonge_config.getAuth() + '/tournaments.json', function (error, response, body) {
+    if (error || response.statusCode != 200) {
+      locals.error = true;
+      return res.render('calendar');
+    } else {
+      let tournaments = JSON.parse(body);
 
-    request(challonge_config.getAuth() + '/tournaments.json', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var events = [];
-            var tournaments = JSON.parse(body);
-
-            for (tournament of tournaments) {
-				if (tournament.tournament.start_at || tournament.tournament.started_at) {
-                    events.push({
-                        id: tournament.tournament.id,
-                        title: tournament.tournament.name,
-                        start: tournament.tournament.start_at || tournament.tournament.started_at, // try timed. will fall back to all-day
-                        end: tournament.tournament.completed_at, // same
-                        url: tournament.tournament.full_challonge_url,
-                        description: tournament.tournament.tournament_type
-                    });
-                    locals.tournaments = events;
-				}
-            }
-        } else {
-            locals.error = true;
-        }
-
-        // Render the view
-        res.render('calendar');
-    });
+      locals.tournaments = tournaments
+        .map(t => t.tournament)
+        .filter(t => t.start_at || t.started_at)
+        .map(t => ({
+          id: t.id,
+          title: t.name,
+          start: t.start_at || t.started_at,
+          end: t.completed_at,
+          url: t.full_challonge_url,
+          description: t.description
+        }));
+      return res.render('calendar');
+    }
+  });
 
 };
