08781cc83374148c894c3ec14383dbdd143fe27f
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-07-23 03:49:54 +0200

    Fix inconsistent indent

--- routes/views/global.js
+++ routes/views/global.js
@@ -1,20 +1,20 @@
 exports = module.exports = function(req, res) {
 
 	var locals = res.locals;
-    var fs = require('fs');
+  var fs = require('fs');
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'competitive';
-  	locals.cSection = 'global';
+  locals.cSection = 'global';
 	locals.ratingTypeTitle = 'Global';
 	locals.ratingType = 'global';
 	locals.apiURL = process.env.API_URL;
-    fs.readFile('members/global.json', 'utf8', function (err, data) {
-        locals.members = JSON.parse(data);
-        locals.lastPage = Math.ceil(locals.members.length / 100);
+  fs.readFile('members/global.json', 'utf8', function (err, data) {
+    locals.members = JSON.parse(data);
+    locals.lastPage = Math.ceil(locals.members.length / 100);
 
-        // Render the view
-        res.render('leaderboards');
-    });
+    // Render the view
+    res.render('leaderboards');
+  });
 };
