a7f27762b970465f1e05e5e6598c0e863e66544e
Rackover <rackover@racknet.noip.me>
2019-01-01 21:21:53 +0000

    Removed the legacy client from the website

--- routes/views/client.js
+++ routes/views/client.js
@@ -2,11 +2,5 @@ exports = module.exports = function(req, res) {
 
 	var locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'client';
-
-	// Render the view
-	res.render('client');
-
+	res.redirect(locals.downlords_faf_client_download_link);
 };
