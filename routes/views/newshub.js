7a35595a2ed0e9740e68cead054e9f49c9f195ad
mike <mjzemek@gmail.com>
2020-05-17 22:13:09 +1000

    Stub for newshub to collaborate with ui team

--- routes/views/newshub.js
+++ routes/views/newshub.js
@@ -7,13 +7,10 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.  
 	locals.section = 'news';
 	locals.title = 'FAForever Newshub';
-	locals.data = {
-		posts: []
-	};
 
     fs.readFile('members/newshub.json', 'utf8', function (err, data) {
         if(err) return;
-        locals.articles = JSON.parse(data);
+        locals.content = JSON.parse(data);
 
         // Render the view
         res.render('newshub');
