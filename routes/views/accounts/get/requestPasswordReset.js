39020d6c9322f70dc70f79bb0c74ffdf562f42ef
Brutus5000 <Brutus5000@gmx.net>
2020-09-18 11:12:56 +0200

    Add password reset via Steam

--- routes/views/accounts/get/requestPasswordReset.js
+++ routes/views/accounts/get/requestPasswordReset.js
@@ -1,3 +1,6 @@
+let request = require('request');
+let error = require('../post/error');
+
 exports = module.exports = function (req, res) {
 
   var locals = res.locals;
@@ -8,9 +11,21 @@ exports = module.exports = function (req, res) {
 
   locals.formData = req.body || {};
 
-  var flash = null;
+  var flash = {};
 
-  // Render the view
-  res.render('account/requestPasswordReset', {flash: flash});
+  let overallRes = res;
+
+  request.post({
+      url: process.env.API_URL + '/users/buildSteamPasswordResetUrl',
+    }, function (err, res, body) {
+
+      if (res.statusCode !== 200) {
+        error.parseApiErrors(body, flash);
+      }
+
+      locals.steamReset = JSON.parse(body).steamUrl
+      overallRes.render('account/requestPasswordReset', {flash: flash});
+    }
+  );
 
 };
