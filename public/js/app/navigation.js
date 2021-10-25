4e6ef46af8361751f2f11ad89f472bbcca2e6dcd
Gatsik <74517072+Gatsik@users.noreply.github.com>
2021-10-25 16:04:31 +0300

    Create Headroom only when header exists

--- public/js/app/navigation.js
+++ public/js/app/navigation.js
@@ -1,19 +1,21 @@
-var headroom = new Headroom(document.querySelector('.navigation-wrapper'), {
-  'offset': 100,
-  'tolerance': 5,
-  'classes': {
-    'initial': '',
-    'pinned': 'pinned',
-    'unpinned': 'unpinned',
-    'top': 'at-top',
-    'notTop': 'not-at-top',
-    'bottom': 'at-bottom',
-    'notBottom': 'not-at-bottom'
-  }
-});
+if (document.querySelector('.navigation-wrapper') !== null) {
+  var headroom = new Headroom(document.querySelector('.navigation-wrapper'), {
+    'offset': 100,
+    'tolerance': 5,
+    'classes': {
+      'initial': '',
+      'pinned': 'pinned',
+      'unpinned': 'unpinned',
+      'top': 'at-top',
+      'notTop': 'not-at-top',
+      'bottom': 'at-bottom',
+      'notBottom': 'not-at-bottom'
+    }
+  });
 
-headroom.init();
-
+  headroom.init();
+}
+  
 $(document).ready(function() {
   $('.toggle-nav').click(function() {
     if ($('.navigation-wrapper').hasClass('opened')) {
