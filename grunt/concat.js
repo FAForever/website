0a51034451547b1a8b08aba2296497201c043f9b
MrRowey <roweyfaf@gmail.com>
2022-03-15 18:08:11 +0000

    Refactoring + Adding Bulk files
    - Removed leftover Boostrap file as we are no longer using the framework.
    - Added New images
    - Updated index.js
    - remove bootstrap line from concat.js

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,7 +3,6 @@ module.exports = {
         files: {
             'public/js/bottom.min.js': [
                 'public/js/jquery/jquery-1.11.3.min.js',
-                'public/js/bootstrap/bootstrap-3.3.5.min.js',
                 'public/js/app/headroom.min.js',
                 'public/js/app/navigation.js'
             ],
