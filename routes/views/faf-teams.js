1fa3f0658c0d044aef2a23d7ec501b963e0d7792
MrRowey <roweyfaf@gmail.com>
2022-03-15 18:59:15 +0000

    Add New Pages with Corrisponting JS Files
    Add blank pages for all the dropdowns & added pages lines in express.js

---
+++ routes/views/faf-teams.js
@@ -0,0 +1,11 @@
+exports = module.exports = function(req, res) {
+
+  let locals = res.locals;
+  
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'faf-teams';
+  res.render('faf-teams');
+  
+
+};
