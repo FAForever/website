3d69fb6fcc188eb41f3b73d6567241b728a72031
MrRowey <roweyfaf@gmail.com>
2022-04-06 22:45:23 +0100

    Game Server Player Counter
    Added back in the player counter to the main page

--- routes/views/index.js
+++ routes/views/index.js
@@ -5,7 +5,22 @@ exports = module.exports = function(req, res) {
   // locals.section is used to set the currently selected
   // item in the header navigation.
   locals.section = 'home';
-  res.render('index');
-  
+
+  let flash = {};
+
+  if(req.query.flash) {
+    let buff = Buffer.from(req.query.flash, 'base64');
+    let text = buff.toString('ascii');
+
+    try {
+      flash = JSON.parse(text);
+    } catch(e) {
+      console.error('Parsing error while trying to decode a flash error: ' + text);
+      console.error(e);
+      flash = [{msg: 'Unknown error'}];
+    }
+  }
+
+  res.render('index', {flash: flash, refreshCountersSeconds: parseInt(process.env.PLAYER_COUNT_UPDATE_INTERVAL)});
 
 };
