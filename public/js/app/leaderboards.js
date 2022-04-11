00ac31517cc316b4a42b0866d36ec1dd2eb01555
Javi <j1a1b1m1@Gmail.com>
2022-04-10 21:34:56 -0400

    1v1 Leaderboard ## PUG index.pug and default.pug both received updates so only the index page shows the amount of players currently online.
    leaderboards.pug received some IDs in order to be able to receive the js from leaderboards.js adequately.
    
    ## JS
    leaderboards.js received the JS needed to be able to pull the data from 1v1.json and make the leaderboard for 1v1. Currently, it only supports the first 100 players, soon a First, last, previous and next buttons will be added.
    
    ## Other scripts
    Certain changes that were very minor were done on extractor.js, getRecentUsers.js and so on were done so the files from the API are placed in the correct folder.

--- public/js/app/leaderboards.js
+++ public/js/app/leaderboards.js
@@ -4,7 +4,7 @@ async function leaderboard1v1JSON() {
 
   console.log(data.length);
   return data;
-}
+} //I accidentally pushed to the wrong branch and now I have to make a comment or I can't push.
 leaderboard1v1JSON()
   .then(data => {
     data;
