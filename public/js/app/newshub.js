23ef965c7e80b7f52e9ea722d2fcdd7f30e945c3
FemboyJavi <j1a1b1m1@gmail.com>
2022-07-25 12:59:09 -0400

    Client News Update
    ## New Features
    Besides now having a much cleaner and smoother design that fits better with FAF, the new newshub comes with a couple more features.
    
    ### No more slowdowns
    The previous newshub was calling the API everytime a user came (as far as I understand the outdated techniques previously used) causing users to wait about 5 seconds for the page to load (both in client and on the website).
    
    The new client newshub caches the newshub in a json file. Meaning users dont have to wait since all the data is already loaded in the website.
    
    ### Image Sizing
    Now all image sizes are supported! No need to make your images 145x97 or other cumbersome sizes. Now images are automatically re-sized.
    
     Images with a 16:9 ratio (2560x1440, 1920x1080, 1280x720 or anything "rectangular") will fit perfectly. Other non-rectangular images will still adjust correctly but might receive a cut-off.
    
     ### Unlimited Articles
     Since now the articles are on a "slider", we dont have a limit with how many articles we can have without sacrificing design. So 5, 7 or 12 articles will all display equally well!
    
     ### Responsiveness
     This design has media queries and responsive unit sizes (such as vw) to always adjust to the users preferences. Therefore, the news will display adequately whether it's on a full screen window or 1/4 of the monitor screen.
    
     ### More Information Less Clutter
     The new design also provides more links for the user. With buttons for joining the discord,  twitch and youtube FAF content being one click away, plus new sections for new players, community tools/spaces and development, plus a Support FAF button and a Report a Bug button all in one compact and clean design! Now Users can have an easier time accessing FAF tools and spaces from the client News!
    
     ## Other
     Many of the images inside the images folder have been moved to a logos folder in order to maintain a better order inside the folder (rather than having 40 images on one folder, have them on their own folders).

--- public/js/app/newshub.js
+++ public/js/app/newshub.js
@@ -1,22 +1,95 @@
-$(document).ready(function() {
+console.log('hello!');
 
-  // when you click on a link within a cell, we only want the link to be clicked, not the cell as well
-  $(".hoverable a").click(function(event) {
-    event.stopPropagation();
-  });
+async function getNewshub() {
+  const response = await fetch(`js/app/json/client-news.json`);
+  const data = await response.json();
+  return await data;
+}
+let dataLength = 0;
+let clientSpawn = document.getElementById('clientSpawn');
+let clientContainer = document.querySelectorAll('.clientContainer');
+let clientMainFeature = document.querySelectorAll('.clientMainFeature');
 
-  // when a cell is clicked, launch the page
-  $(".hoverable").click(function() {
+function createArticles() {
+  getNewshub()
+    .then(data => {
+      dataLength = data.length;
+      let fixedLinkingOrder = data.length - 1;
+      for (let i = 0; i < data.length - 1; i++) {
+        clientSpawn.insertAdjacentHTML('afterbegin', `<a target='_blank' href="${data[fixedLinkingOrder].link}">
+    <div class="clientContainer column1">
+        <div class="clientImage"></div>
+        <div class="clientText">
+            <h1 class="clientTitle"></h1>
+            <div class="clientContent"></div>
+        </div>
+    </div>
+</a>`);
 
-    var url = $(this).attr("data-url");
-    if (typeof window.java !== 'undefined')
-    {
-      window.java.openUrl(url); // when hosted in java client, makes it launch in a new window
+        fixedLinkingOrder--;
+      }
+      clientMainFeature[0].insertAdjacentHTML('afterbegin', `<a class="featureSubGrid column9" target='_blank' href="${data[0].link}">
+    <div class="featureContainer column5">
+        <div class="featureImage"></div>
+    </div>
+    <div class="featureContainer column7">
+        <div class="featureText">
+            <h1 class="featureTitle"></h1>
+            <div class="featureContent"></div>
+        </div>
+    </div>
+</a>`);
+      return data;
+    }).then(data => {
+
+    let clientImage = document.querySelectorAll('.clientImage');
+    let clientTitle = document.querySelectorAll('.clientTitle');
+    let clientContent = document.querySelectorAll('.clientContent');
+    for (let i = 0; i < data.length - 1; i++) {
+      let content = data[i + 1].content;
+      clientImage[i].style.backgroundImage = `url("${data[i + 1].media}")`;
+      clientTitle[i].innerHTML = `${data[i + 1].title}`;
+      clientContent[i].innerHTML = `${content.substring(0, 200)}`;
     }
-    else
-    {
-      window.open(url, "_blank"); // normal web browser change
-    }
+    let featureImage = document.querySelectorAll('.featureImage');
+    let featureTitle = document.querySelectorAll('.featureTitle');
+    let featureContent = document.querySelectorAll('.featureContent');
+    let content = data[0].content;
+    featureImage[0].style.backgroundImage = `url("${data[0].media}")`;
+    featureTitle[0].innerHTML = `${data[0].title}`;
+    featureContent[0].innerHTML = `${content.substring(0, 350)}`;
+
   });
+}
+
+createArticles();
+let arrowRight = document.getElementById('clientArrowRigth');
+let arrowLeft = document.getElementById('clientArrowLeft');
+let newsPosition = 0;
+let newsLimit = 0;
+let newsMove = clientContainer[0].offsetWidth;
+console.log(newsMove)
+arrowRight.addEventListener('click', () => {
+
+  if (newsLimit === dataLength - 1) {
+    console.log('limit reached')
+  }else {
+    newsLimit++;
+    newsPosition = newsPosition - newsMove;
+    clientSpawn.style.transform = `translateX(${newsPosition}px)`;
+    arrowLeft.style.display = 'grid';
+  }
 
 });
+
+arrowLeft.addEventListener('click', () => {
+  if (newsLimit === 0) {
+  }else {
+    newsLimit--;
+    newsPosition = newsPosition + newsMove;
+    clientSpawn.style.transform = `translateX(${newsPosition + 5}px)`;
+  }
+
+
+});
+
--- scripts/extractor.js
+++ scripts/extractor.js
@@ -92,7 +92,7 @@ module.exports.run = function run() {
 
       saveLeaderboardRatingsToFile(ratings, "members/2v2.json");
     });
-    
+
     models.reset();
 
     request(process.env.API_URL + "/data/leaderboardRating?include=player&sort=-rating&filter=leaderboard.id==4;updateTime=ge=" +
@@ -114,3 +114,51 @@ module.exports.run = function run() {
     console.log(e);
   }
 };
+
+require("dotenv").config();
+
+const fetch = require('node-fetch');
+let d = new Date();
+let timeFilter = 6;
+let minusTimeFilter = d.setMonth(d.getMonth() - timeFilter);
+let currentDate = new Date(minusTimeFilter).toISOString();
+
+async function getClientNews() {
+  let response = await fetch(`https://direct.faforever.com/wp-json/wp/v2/posts/?per_page=10&_embed&_fields=_links.author,_links.wp:featuredmedia,_embedded,title,newshub_externalLinkUrl,newshub_sortIndex,content.rendered,date,categories&categories=283`);
+  let data = await response.json();
+  //Now we get a js array rather than a js object. Otherwise we can't sort it out.
+  let dataObjectToArray = Object.values(data);
+  let sortedData = dataObjectToArray.map(item => ({
+    category: item.categories,
+    sortIndex: item.newshub_sortIndex,
+    link: item.newshub_externalLinkUrl,
+    date: item.date,
+    title: item.title.rendered,
+    content: item.content.rendered,
+    author: item._embedded.author[0].name,
+    media: item._embedded['wp:featuredmedia'][0].source_url,
+  }));
+  sortedData.sort((articleA, articleB) => articleB.sortIndex - articleA.sortIndex);
+
+  function onlyActiveArticles(article) {
+    return article.category[1] !== 284;
+  }
+
+  let clientNewsData = sortedData.filter(onlyActiveArticles);
+  return await clientNewsData;
+}
+
+module.exports.run = function run() {
+  getClientNews()
+    .then(clientNewsData => {
+      fs.writeFile(`public/js/app/json/client-news.json`, JSON.stringify(clientNewsData), error => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(`${currentDate} - Client News file created successfully.`);
+        }
+      });
+    });
+};
+
+
