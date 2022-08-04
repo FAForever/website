206a325321d23e2822cae3ff34966f626938f963
FemboyJavi <j1a1b1m1@gmail.com>
2022-08-03 21:53:25 -0400

    Donations Page
    ## Page Creation
    The Donation has been updated. Now featuring a slicker design that elegantly incorporates the new background.
    
    The page now has a pie chart to demonstrate how FAF expenses are being done and two buttons for Patreon and Paypal.

---
+++ public/js/app/donation.js
@@ -0,0 +1,75 @@
+
+Highcharts.chart('container', {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: true,
+    backgroundColor: 'transparent',
+    type: 'pie',
+
+  },
+  credits: {
+    enabled: false
+  },
+  exporting: {
+    enabled: false
+  },
+  title: {
+    text: 'How FAF Uses Donations',
+    style: {
+      color: '#ffffff',
+      fontSize: '30px',
+      fontFamily: 'electrolize'
+    }
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  accessibility: {
+    point: {
+      valueSuffix: '%'
+    }
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: false,
+      cursor: 'pointer',
+      
+      dataLabels: {
+        color: '#ffffff',
+        font: '20px electrolize',
+        fontSize:'20px',
+        enabled: true,
+        format: '<h2>{point.name}</h2>: {point.percentage:.1f} %',
+        style: {
+          fontSize: '18px',
+          fontFamily: 'electrolize'
+        },
+      }
+    }
+  },
+  dataLabels: {
+    style: {
+      color: '#ffffff'
+    }
+  },
+  series: [{
+    name: 'Expenses',
+    colorByPoint: true,
+    color: '#ffffff',
+    
+    data: [{
+      name: 'Infrastructure Costs',
+      y: 56,
+      sliced: true,
+      color: '#7376a8',
+      selected: true
+      
+    }, {
+      name: 'Tournament Prizes',
+      y: 44,
+      color: '#dadada',
+      selected: true
+    }]
+  }]
+});
--- routes/views/donation.js
+++ routes/views/donation.js
@@ -1,4 +1,8 @@
+
 exports = module.exports = function(req, res) {
+
+
+
 
   let locals = res.locals;
 	// locals.section is used to set the currently selected item in the header navigation.
