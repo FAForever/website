25f409c7b2f76dd3b9973380d2dd4caae889fe6e
FemboyJavi <j1a1b1m1@gmail.com>
2022-09-30 11:20:54 -0400

    Backend Work ### Backend Changes This patch may not add much in the test server at the moment, but its a way to save the changes I'm doing while using discord as my oauth2 provider. I would add more detailled patches but this build is a bit experimental/learning so its very likely it will receive heavy changes.

--- express.js
+++ express.js
@@ -1,43 +1,20 @@
-// Simulate config options from your production environment by
-// customising the .env file in your project's root folder.
-require('dotenv').config();
-//Define environment variables with default values
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-process.env.PORT = process.env.PORT || '4000';
-process.env.OAUTH_URL = process.env.OAUTH_URL || 'https://hydra.test.faforever.com';
-process.env.API_URL = process.env.API_URL || 'https://api.faforever.com';
-process.env.OAUTH_CLIENT_ID = process.env.OAUTH_CLIENT_ID || '12345';
-process.env.OAUTH_CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET || '12345';
-process.env.HOST = process.env.HOST || 'http://localhost';
-process.env.SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY || '12345';
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const mongoStore = require('connect-mongo')
+const bodyParser = require('body-parser')
+require('./scripts/discord');
+require('./scripts/database');
+const app = express();
 
-
-
-let express = require('express');
-let middleware = require('./routes/middleware');
-let bodyParser = require('body-parser');
-let passport = require('passport');
-let OidcStrategy = require('passport-openidconnect');
-let flash = require('connect-flash');
-const cors = require('cors');
-let app = express();
-
-//Start and listen on port
-app.listen(process.env.PORT,() => {
-  console.log('Express listening on port ' + process.env.PORT);
-});
+//Routes
+const newsRoute = require('./scripts/getNews');
 
 //Initialize values for default configs
 app.set('views', 'templates/views');
 app.set('view engine', 'pug');
-app.set('port', process.env.PORT);
-
-app.locals.clanInvitations = {};
-
-//Execute middleware before each request...
-app.use(middleware.initLocals);
-app.use(middleware.getLatestClientRelease);
-app.use(middleware.clientChecks);
+app.set('port', 3000);
 
 
 //Set static public directory path
@@ -46,203 +23,57 @@ app.use(express.static('public', {
   maxAge: 4 * 60 * 60 * 1000 // 4 hours
 }));
 
-app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
 
-app.use(require('express-session')({
-  secret: process.env.SESSION_SECRET_KEY,
+app.use(session({
+  secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    maxAge: process.env.TOKEN_LIFESPAN * 1000
-  }
+
+  maxAge: 1000 * 60,
+  /*
+  store: mongoStore.create({
+    mongoUrl: 'mongodb+srv://Javi:4321@cluster0.j52qwuz.mongodb.net/test'
+  })
+   */
 }));
 
-//Authentication on pages
+
+
+
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
-app.use(middleware.username);
+app.use(newsRoute);
 
 
+//Start and listen on port
+app.listen(3000, () => {
+  console.log('Express listening on port ' + 3000);
+});
 
+app.get('/login', passport.authenticate('discord'));
 
-
-//Define routes
-
-
-function loggedIn(req, res, next) {
-  // Takes the current URL
-  req.session.referral = req.protocol + '://' + req.get('host') + req.originalUrl;
-  if (req.isAuthenticated()) {
-    res.locals.username = req.user.data.attributes.userName;
-    next();
-    
-  } 
-  // Not logged? Gets sent to /login
-  else {
-    res.redirect('/login');
-  }
-}
-
-
-//Variable to hold routing path
-let routes = './routes/views/';
-
-app.get('/account/register', require(routes + 'accounts/get/register'));
-app.post('/account/register', require(routes + 'accounts/post/register'));
-
-app.get('/account/activate', require(routes + 'accounts/get/activate'));
-app.post('/account/activate', require(routes + 'accounts/post/activate'));
-
-// Callback for registration to create the account using a token
-app.get('/account/create', require(routes + 'accounts/get/createAccount'));
-
-app.get('/account/link', loggedIn, require(routes + 'accounts/get/linkSteam'));
-app.get('/account/resync', loggedIn, require(routes + 'accounts/get/resync'));
-
-app.get('/account/connect', loggedIn, require(routes + 'accounts/get/connectSteam'));
-
-app.get('/account/password/reset', require(routes + 'accounts/get/requestPasswordReset'));
-app.post('/account/password/reset', require(routes + 'accounts/post/requestPasswordReset'));
-
-app.get('/account/password/confirmReset', require(routes + 'accounts/get/confirmPasswordReset'));
-app.post('/account/password/confirmReset', require(routes + 'accounts/post/confirmPasswordReset'));
-
-app.get('/account/report', loggedIn, require(routes + 'accounts/get/report'));
-app.post('/account/report', loggedIn, require(routes + 'accounts/post/report'));
-
-app.get('/account/password/change', loggedIn, require(routes + 'accounts/get/changePassword'));
-app.post('/account/password/change', loggedIn, require(routes + 'accounts/post/changePassword'));
-
-app.get('/account/username/change', loggedIn, require(routes + 'accounts/get/changeUsername'));
-app.post('/account/username/change', loggedIn, require(routes + 'accounts/post/changeUsername'));
-
-app.get('/account/email/change', loggedIn, require(routes + 'accounts/get/changeEmail'));
-app.post('/account/email/change', loggedIn, require(routes + 'accounts/post/changeEmail'));
-
-app.get('/account_activated', require(routes + 'accounts/get/register'));
-app.get('/password_resetted', require(routes + 'accounts/get/requestPasswordReset'));
-app.get('/report_submitted', require(routes + 'accounts/get/report'));
-
-
+app.get('/login/redirect/', passport.authenticate('discord', {
+  failureRedirect: '/',
+}), function(req, res) {
+  console.log('You did it!');
+  res.redirect('/newshub'); // Successful auth
+});
 //All Pages
-// when the website is asked to render "/pageName" it will come here and see what are the "instructions" to render said page. If the page isn't here, then the website won't render it properly.    
+// when the website is asked to render "/pageName" it will come here and see what are the "instructions" to render said page. If the page isn't here, then the website won't render it properly.
 let appGetRouteArray = [
   // This first '' is the home/index page
-  '',
-  'client-news',
-  'newshub',
-  'campaign-missions',
-  'scfa-vs-faf',
-  'donation',
-  'tutorials-guides',
-  'ai',
-  'patchnotes',
-  'faf-teams',
-  'contribution',
-  'content-creators',
-  'tournaments',
-  'training',
-  'leaderboards',
-  'play',
-  'tos',
-  'tos-ru',
-  'tos-fr',
-  'newsArticle',
+  '', 'client-news', 'newshub', 'campaign-missions', 'scfa-vs-faf', 'donation', 'tutorials-guides', 'ai', 'patchnotes', 'faf-teams', 'contribution', 'content-creators', 'tournaments', 'training', 'leaderboards', 'play', 'tos', 'tos-ru', 'tos-fr', 'newsArticle',
 ];
 //Renders every page written above
-appGetRouteArray.forEach(page => app.get(`/${page}`, (req,res) => {
+appGetRouteArray.forEach(page => app.get(`/${page}`, (req, res) => {
   res.render(page);
 }));
 
-
-
-
-app.get('/account/checkUsername', require('./routes/views/accounts/get/checkUsername'));
-
-// Clans
-app.get('/clans', require(routes + 'clans/get/index'));
-app.get('/clans/create', loggedIn, require(routes + 'clans/get/create'));
-app.get('/clans/manage', loggedIn, require(routes + 'clans/get/manage'));
-app.get('/clans/see', require(routes + 'clans/get/see'));
-app.get('/clans/browse', require(routes + 'clans/get/browse'));
-app.get('/clans/accept', loggedIn, require(routes + 'clans/get/accept'));
-
-app.post('/clans/create', loggedIn, require(routes + 'clans/post/create'));
-app.post('/clans/destroy', loggedIn, require(routes + 'clans/post/destroy'));
-app.post('/clans/invite', loggedIn, require(routes + 'clans/post/invite'));
-app.post('/clans/kick', loggedIn, require(routes + 'clans/post/kick'));
-app.post('/clans/transfer', loggedIn, require(routes + 'clans/post/transfer'));
-app.post('/clans/update', loggedIn, require(routes + 'clans/post/update'));
-app.post('/clans/leave', loggedIn, require(routes + 'clans/post/leave'));
-
-// Get any clan, see that clan id
-app.get('/clan/*', function (req, res) {
-  const id = req.path.split('/').slice(-1)[0];
-  res.redirect('/clans/see?id=' + id);
-
-});
-//Logout function
-app.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
-
-app.get('/login', passport.authenticate('faforever', {
-  failureRedirect: '/login',
-  failureFlash: true
-}), function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
-
-app.get('/callback', passport.authenticate('faforever', {
-  failureRedirect: '/login',
-  failureFlash: true
-}), function (req, res) {
-  res.redirect(req.session.referral ? req.session.referral : '/');
-  req.session.referral = null;
-});
-
-//Strategy in current use?
-passport.use('faforever', new OidcStrategy({
-    issuer: process.env.OAUTH_URL + '/',
-    tokenURL: process.env.OAUTH_URL + '/oauth2/token',
-    authorizationURL: process.env.OAUTH_URL + '/oauth2/auth',
-    userInfoURL: process.env.OAUTH_URL + '/userinfo?schema=openid',
-    clientID: process.env.OAUTH_CLIENT_ID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    callbackURL: process.env.HOST + '/callback',
-    scope: ['openid', 'public_profile', 'write_account_data']
-  },
-  function (iss, sub, profile, jwtClaims, accessToken, refreshToken, params, verified) {
-    let request = require('request');
-    request.get(
-      {url: process.env.API_URL + '/me', headers: {'Authorization': 'Bearer ' + accessToken}},
-      function (e, r, body) {
-        if (r.statusCode !== 200) {
-          return verified(null);
-        }
-        let user = JSON.parse(body);
-        user.data.attributes.token = accessToken;
-        user.data.id = user.data.attributes.userId;
-        return verified(null, user);
-      }
-    );
-  }
-));
-
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (id, done) {
-  done(null, id);
-});
-
-const newsRoute = require('./scripts/getNews');
-app.use(newsRoute);
+/*
 
 // Run scripts initially on startup
 let requireRunArray = ['extractor', 'getLatestClientRelease'];
@@ -259,7 +90,7 @@ for (let i = 0; i < requireRunArray.length; i++) {
     } catch (e) {
       console.error(`${requireRunArray[i]} caused the error`, e);
     }
-  },  1000 * 60 * 5); // 5 Minutes
+  }, 1000 * 60 * 5); // 5 Minutes
 }
 setInterval(() => {
   try {
@@ -268,9 +99,7 @@ setInterval(() => {
   } catch (e) {
     console.error(`getRecentUsers script caused the error`, e);
   }
-},  1000 * 15); // 30 seconds
-
-
+}, 1000 * 30); // 30 seconds
 
 
 //404 Error Handlers
@@ -279,18 +108,5 @@ app.use(function (req, res) {
 });
 
 
+ */
 
-//Display 500 Error Handler if in development mode.
-if (process.env.NODE_ENV === 'development') {
-  app.enable('verbose errors');
-
-  //500 Error Handler
-  app.use(function (err, req, res) {
-    res.status(500).render('errors/500', {error: err});
-  });
-}
-
-/*app.use( (err, req, res, next) => {
-  res.status(!200).render('error/error');
-});
-*/
---
+++ scripts/discord.js
@@ -0,0 +1,77 @@
+const passport = require('passport');
+const DiscordStrategy = require('passport-discord').Strategy;
+const scopes = ['identify', 'email', 'guilds.join'];
+const discordUser = require("../scripts/database/schemas/discordSchema");
+
+/*
+passport.serializeUser(function(user, done) {
+  console.log('serialize user');
+  console.log(user);
+  done(null, user);
+});
+
+passport.deserializeUser(async function(id, done) {
+  console.log('deserialize user');
+  try {
+    const user = await discordUser.findById(id);
+    console.log(user);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    done(null, user);
+  } catch (err) {
+    console.log(err);
+    done(err, null);
+  }
+});
+
+ */
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    console.log('serialize User');
+    return cb(null, {id: user.id, username: user.username});
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  console.log('Deserialize User');
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+
+passport.use(
+  new DiscordStrategy(
+    
+    {
+      clientID: '1021451644573061220',
+      clientSecret: '_RoGE_iFWiwclB6Xjn-7kuci3xrXwuYu',
+      callbackURL: 'http://localhost:3000/login/redirect',
+      scope: scopes
+    },(
+      async (accessToken, refreshToken, profile, done) => {
+        //console.log(accessToken, refreshToken, profile);
+        try {
+          const discordUserData = await discordUser.findOne({discordId: profile.id});
+          if (discordUserData) {
+            console.log('Found user');
+            return done(null, discordUserData);
+          } else {
+            console.log('No user found');
+            const newUser = await discordUser.create({
+              discordId: profile.id,
+              username: profile.username,
+
+            });
+            console.log('New user created!');
+            return done(null, newUser);
+          }
+        } catch (err) {
+          console.log('There was an error!');
+          return done(err, null);
+        }
+      }
+
+    )
+  ));
