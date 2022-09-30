ea5555d40944406042d63a8fda71c4ff29c2bc05
FemboyJavi <j1a1b1m1@gmail.com>
2022-09-30 12:11:54 -0400

    Backend Work ### Backend Changes This patch may not add much in the test server at the moment, but its a way to save the changes I'm doing while using discord as my oauth2 provider. I would add more detailled patches but this build is a bit experimental/learning so its very likely it will receive heavy changes.

--- scripts/database/index.js
+++ scripts/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://Javi:4321@cluster0.j52qwuz.mongodb.net/test')
+    .connect('mongo')
     .then(() => console.log('connected to db'))
     .catch((err)=>console.log(err))
--- scripts/discord.js
+++ scripts/discord.js
@@ -26,16 +26,16 @@ passport.deserializeUser(async function(id, done) {
 });
 
  */
-passport.serializeUser(function(user, cb) {
-  process.nextTick(function() {
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
     console.log('serialize User');
     return cb(null, {id: user.id, username: user.username});
   });
 });
 
-passport.deserializeUser(function(user, cb) {
+passport.deserializeUser(function (user, cb) {
   console.log('Deserialize User');
-  process.nextTick(function() {
+  process.nextTick(function () {
     return cb(null, user);
   });
 });
@@ -43,13 +43,12 @@ passport.deserializeUser(function(user, cb) {
 
 passport.use(
   new DiscordStrategy(
-    
     {
-      clientID: '1021451644573061220',
-      clientSecret: '_RoGE_iFWiwclB6Xjn-7kuci3xrXwuYu',
+      clientID: 'id',
+      clientSecret: 'secret',
       callbackURL: 'http://localhost:3000/login/redirect',
       scope: scopes
-    },(
+    }, (
       async (accessToken, refreshToken, profile, done) => {
         //console.log(accessToken, refreshToken, profile);
         try {
@@ -62,7 +61,6 @@ passport.use(
             const newUser = await discordUser.create({
               discordId: profile.id,
               username: profile.username,
-
             });
             console.log('New user created!');
             return done(null, newUser);
