b5f24da07e76c3d22f94ff373baea37328bb457e
FemboyJavi <j1a1b1m1@gmail.com>
2022-10-09 12:54:42 -0400

    extractor.js env and error handling
    ### dotenv and extractor.js
    Extractor js now uses the env configuration on its API calls but also features its own default values in case they arent specified in the ENV.
    
    ### Error Handling
    All extractor.js calls now can handle errors and not break the whole application/website.

--- scripts/extractor.js
+++ scripts/extractor.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+process.env.API_URL = process.env.API_URL || 'https://api.faforever.com';
+process.env.WP_URL = process.env.WP_URL || 'https:direct.faforever.com';
 
 const fs = require('fs');
 const fetch = require('node-fetch');
@@ -11,106 +13,145 @@ let currentDate = new Date(minusTimeFilter).toISOString();
 
 //TODO: Make an env variable for all API/Wordpress urls, rather than using direct values
 async function newshub() {
-  let response = await fetch(`https:direct.faforever.com/wp-json/wp/v2/posts/?per_page=100&_embed&_fields=_links.author,_links.wp:featuredmedia,_embedded,title,content.rendered,date,categories&categories=587`);
-  let fetchData = await response.json();
-  //Now we get a js array rather than a js object. Otherwise we can't sort it out.
-  let dataObjectToArray = Object.values(fetchData);
-  let data = dataObjectToArray.map(item => ({
-    date: item.date,
-    title: item.title.rendered,
-    content: item.content.rendered,
-    author: item._embedded.author[0].name,
-    media: item._embedded['wp:featuredmedia'][0].source_url,
-  }));
-  return await data;
+
+  try {
+    let response = await fetch(`${process.env.WP_URL}/wp-json/wp/v2/posts/?per_page=100&_embed&_fields=_links.author,_links.wp:featuredmedia,_embedded,title,content.rendered,date,categories&categories=587`);
+    let fetchData = await response.json();
+    //Now we get a js array rather than a js object. Otherwise we can't sort it out.
+    let dataObjectToArray = Object.values(fetchData);
+    let data = dataObjectToArray.map(item => ({
+      date: item.date,
+      title: item.title.rendered,
+      content: item.content.rendered,
+      author: item._embedded.author[0].name,
+      media: item._embedded['wp:featuredmedia'][0].source_url,
+    }));
+    return await data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+
+
 }
 
 async function clientNews() {
-  let response = await fetch(`https:direct.faforever.com/wp-json/wp/v2/posts/?per_page=10&_embed&_fields=_links.author,_links.wp:featuredmedia,_embedded,title,newshub_externalLinkUrl,newshub_sortIndex,content.rendered,date,categories&categories=283`);
-  let fetchData = await response.json();
-  let dataObjectToArray = Object.values(fetchData);
-  let sortedData = dataObjectToArray.map(item => ({
-    category: item.categories,
-    sortIndex: item.newshub_sortIndex,
-    link: item.newshub_externalLinkUrl,
-    date: item.date,
-    title: item.title.rendered,
-    content: item.content.rendered,
-    author: item._embedded.author[0].name,
-    media: item._embedded['wp:featuredmedia'][0].source_url,
-  }));
-  sortedData.sort((articleA, articleB) => articleB.sortIndex - articleA.sortIndex);
 
-  function onlyActiveArticles(article) {
-    return article.category[1] !== 284;
+  try {
+    let response = await fetch(`${process.env.WP_URL}/wp-json/wp/v2/posts/?per_page=10&_embed&_fields=_links.author,_links.wp:featuredmedia,_embedded,title,newshub_externalLinkUrl,newshub_sortIndex,content.rendered,date,categories&categories=283`);
+    let fetchData = await response.json();
+    let dataObjectToArray = Object.values(fetchData);
+    let sortedData = dataObjectToArray.map(item => ({
+      category: item.categories,
+      sortIndex: item.newshub_sortIndex,
+      link: item.newshub_externalLinkUrl,
+      date: item.date,
+      title: item.title.rendered,
+      content: item.content.rendered,
+      author: item._embedded.author[0].name,
+      media: item._embedded['wp:featuredmedia'][0].source_url,
+    }));
+    sortedData.sort((articleA, articleB) => articleB.sortIndex - articleA.sortIndex);
+
+    function onlyActiveArticles(article) {
+      return article.category[1] !== 284;
+    }
+
+    let data = sortedData.filter(onlyActiveArticles);
+    return await data;
+  } catch (e) {
+    console.log(e);
+    return null;
   }
-
-  let data = sortedData.filter(onlyActiveArticles);
-  return await data;
 }
 
 
 async function fafTeams() {
-  let response = await fetch(`https:direct.faforever.com/wp-json/wp/v2/posts/?per_page=100&_embed&_fields=content.rendered,categories&categories=636`);
-  let fetchData = await response.json();
-  let dataObjectToArray = Object.values(fetchData);
-  let data = dataObjectToArray.map(item => ({
-    content: item.content.rendered,
-  }));
-  return await data;
+
+  try {
+    let response = await fetch(`${process.env.WP_URL}/wp-json/wp/v2/posts/?per_page=100&_embed&_fields=content.rendered,categories&categories=636`);
+    let fetchData = await response.json();
+    let dataObjectToArray = Object.values(fetchData);
+    let data = dataObjectToArray.map(item => ({
+      content: item.content.rendered,
+    }));
+    return await data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+
 }
 
 async function contentCreators() {
-  let response = await fetch(`https:direct.faforever.com/wp-json/wp/v2/posts/?per_page=100&_embed&_fields=content.rendered,categories&categories=639`);
-  let fetchData = await response.json();
-  let dataObjectToArray = Object.values(fetchData);
-  let data = dataObjectToArray.map(item => ({
-    content: item.content.rendered,
-  }));
-  return await data;
+  try {
+    let response = await fetch(`${process.env.WP_URL}/wp-json/wp/v2/posts/?per_page=100&_embed&_fields=content.rendered,categories&categories=639`);
+    let fetchData = await response.json();
+    let dataObjectToArray = Object.values(fetchData);
+    let data = dataObjectToArray.map(item => ({
+      content: item.content.rendered,
+    }));
+    return await data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+
+
 }
 
 async function getAllClans() {
-  let response = await fetch(`https://api.faforever.com/data/clan?fields[clan]=name,tag&page[number]=1&page[size]=3000`);
-  let fetchData = await response.json();
-  let dataObjectToArray = Object.values(fetchData);
-  let data = dataObjectToArray[0].map((item) => ({
-    id: item.id,
-    name: item.attributes.name,
-    tag: item.attributes.tag,
-  }));
-  return await data;
+  try {
+    let response = await fetch(`${process.env.API_URL}/data/clan?fields[clan]=name,tag&page[number]=1&page[size]=3000`);
+    let fetchData = await response.json();
+    let dataObjectToArray = Object.values(fetchData);
+    let data = dataObjectToArray[0].map((item) => ({
+      id: item.id,
+      name: item.attributes.name,
+      tag: item.attributes.tag,
+    }));
+    return await data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+
 }
 
 
 async function getLeaderboards(leaderboardID) {
-  let response = await fetch(`https://api.faforever.com/data/leaderboardRating?include=player&sort=-rating&filter=leaderboard.id==${leaderboardID};updateTime=ge=${currentDate}&page[size]=9999`);
+  try {
+    let response = await fetch(`${process.env.API_URL}/data/leaderboardRating?include=player&sort=-rating&filter=leaderboard.id==${leaderboardID};updateTime=ge=${currentDate}&page[size]=9999`);
 
-  let data = await response.json();
-  let dataObjectToArray = await Object.values(data);
-  
-  let playerLogin = dataObjectToArray[2].map(item => ({
-    label: item.attributes.login
-  }));
-  let playerValues = dataObjectToArray[0].map(item => ({
-    rating: item.attributes.rating,
-    totalgames: item.attributes.totalGames,
-    wonGames: item.attributes.wonGames,
-    date: item.attributes.updateTime,
-  }));
-  const combineArrays = (array1, array2) => array1.map((x, i) => [x, array2[i]]);
-  let leaderboardData = combineArrays(playerLogin, playerValues);
-  leaderboardData.sort((playerA, playerB) => playerA[1].rating - playerB[1].rating);
-  return await leaderboardData;
+    let data = await response.json();
+    let dataObjectToArray = await Object.values(data);
+
+    let playerLogin = dataObjectToArray[2].map(item => ({
+      label: item.attributes.login
+    }));
+    let playerValues = dataObjectToArray[0].map(item => ({
+      rating: item.attributes.rating,
+      totalgames: item.attributes.totalGames,
+      wonGames: item.attributes.wonGames,
+      date: item.attributes.updateTime,
+    }));
+    const combineArrays = (array1, array2) => array1.map((x, i) => [x, array2[i]]);
+    let leaderboardData = combineArrays(playerLogin, playerValues);
+    leaderboardData.sort((playerA, playerB) => playerA[1].rating - playerB[1].rating);
+    return await leaderboardData;
+  } catch (e) {
+    console.log(e);
+    return null;
+
+  }
 }
 
 module.exports.run = function run() {
-  //we start at 2 because we want the ID starting on 2
-  //Leaderboards gets its own call since it needs a for loop, 
-  
+
+  // Do not change the order of these/make sure they match the order of fileNames below
   const extractorFunctions = [
     newshub(), contentCreators(), clientNews(), fafTeams(), getAllClans(),
-    getLeaderboards(1),getLeaderboards(2),getLeaderboards(3),getLeaderboards(4),  
+    getLeaderboards(1), getLeaderboards(2), getLeaderboards(3), getLeaderboards(4),
   ];
   //Make sure to not change the order of these since they match the order of extractorFunctions
   const fileNames = [
@@ -125,7 +166,7 @@ module.exports.run = function run() {
           if (error) {
             console.log(error);
           } else {
-            console.log(`${currentDate} - ${fileName} file created successfully.`);
+            console.log(`${currentDate} - ${fileName} file created.`);
           }
         });
       });
