d04b7a3826c5448bd3a5fcf1719e5dc08451d671
FemboyJavi <j1a1b1m1@gmail.com>
2022-10-22 18:41:38 -0400

    Test Server Commit ### Explanation This commit will not have a message about what has been changed since its being done just to test on the test server.
    This means I'll be changing small lines of code, commiting and then building the test server, seeing whats happening and repeat.

--- routes/views/account/post/changeUsername.js
+++ routes/views/account/post/changeUsername.js
@@ -2,9 +2,9 @@ let flash = {};
 let request = require('request');
 let error = require('./error');
 const {check, validationResult} = require('express-validator');
-
+console.log('changeUsername logged for the first time!');
 exports = module.exports = function (req, res) {
-
+  console.log('1 - changeUsername is being read!');
     let locals = res.locals;
 
     locals.formData = req.body || {};
@@ -27,22 +27,24 @@ exports = module.exports = function (req, res) {
         res.render('account/changeUsername', {flash: flash});
 
     } else {
-
+      console.log('2 - changeUsername is being read!');
     let username = req.body.username;
     let overallRes = res;
-
+      console.log('3 - changeUsername is being read!');
     //Run post to reset endpoint
     request.post({
       url: process.env.API_URL + '/users/changeUsername',
       headers: {'Authorization': 'Bearer ' + req.user.token},
       form: {newUsername: username}
     }, function (err, res, body) {
-
+      console.log('4 - changeUsername is being read!');
+      
+      
       if (res.statusCode !== 200) {
         error.parseApiErrors(body, flash);
         return overallRes.render('account/changeUsername', {flash: flash});
       }
-
+      console.log('5 - changeUsername is being read!');
       // Successfully changed username
       flash.class = 'alert-success';
       flash.messages = [{msg: 'Your username was changed successfully. Please use the new username to log in!'}];
