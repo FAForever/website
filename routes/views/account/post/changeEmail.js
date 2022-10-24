64be1443f16e25c83dd001535cc9e6847d622ffa
FemboyJavi <j1a1b1m1@gmail.com>
2022-10-24 12:07:35 -0400

    Test Server Commit

--- routes/views/account/post/changeEmail.js
+++ routes/views/account/post/changeEmail.js
@@ -43,26 +43,23 @@ exports = module.exports = function (req, res) {
 
 //TODO: Axios is giving me a 400 error
 
-    request.post({
-      url: `${process.env.API_URL}/users/changeEmail`,
+    console.log('Axios is being used');
+    axios.post(`${process.env.API_URL}/users/changeEmail`, {
       headers: {'Authorization': `Bearer ${req.user.token}`},
-      form: {newEmail: email, currentPassword: password}
-    }, function (err, res, body) {
+      data: {newEmail: req.body.email, currentPassword: req.body.password}
 
-      if (res.statusCode !== 200) {
-        error.parseApiErrors(body, flash);
-        return overallRes.render('account/changeEmail', {flash: flash});
-      }
+    }).then((response) => {
+      console.log('.then()');
+      console.log(response);
+      
 
       // Successfully changed email
       flash.class = 'alert-success';
       flash.messages = [{msg: 'Your email was set successfully.'}];
       flash.type = 'Success!';
-
       overallRes.render('account/changeEmail', {flash: flash});
+
     });
+    
   }
 };
-
-
-
