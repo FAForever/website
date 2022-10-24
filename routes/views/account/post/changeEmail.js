202de6db263c09fbea3b412a8b7a0fe4bc5daeff
FemboyJavi <j1a1b1m1@gmail.com>
2022-10-24 11:45:03 -0400

    Test Server Commit

--- routes/views/account/post/changeEmail.js
+++ routes/views/account/post/changeEmail.js
@@ -31,16 +31,19 @@ exports = module.exports = function (req, res) {
   } else {
 
     // pull the form variables off the request body
-    let email = req.body.email;
-    let password = req.body.password;
+   
     let overallRes = res;
+    let formEmailData = {
+      newEmail: req.body.email,
+      currentPassword: req.body.password,
+    };
 
 //TODO: Axios is giving me a 400 error
 
     console.log('Axios is being used');
     axios.post(`${process.env.API_URL}/users/changeEmail`, {
       headers: {'Authorization': `Bearer ${req.user.token}`},
-      data: {newEmail: email, currentPassword: password}
+      form: formEmailData
 
     }).then((response) => {
       console.log('.then()');
