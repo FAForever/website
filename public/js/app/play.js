926ddea677dd211600552d5f7ff9c4ebb2c4ecd1
fcaps <alex@fcaps.net>
2023-11-12 02:32:12 +0100

    move github link to the browser, so server-startup will not fail due a rate-limit of the github-api #418

---
+++ public/js/app/play.js
@@ -0,0 +1,54 @@
+import {Octokit} from "https://esm.sh/@octokit/core";
+
+const githubOrg = 'faforever'
+const githubRepository = 'downlords-faf-client'
+const githubFallbackUrl = 'https://github.com/FAForever/downlords-faf-client/releases';
+const downloadButtonId = 'faf-client-download'
+
+const startDownloadFile = (url) => window.location.assign(url)
+
+const openFallbackDownloadPage = () => open(githubFallbackUrl, '_blank')
+
+const getWindowsDownloadLink = (response) => {
+    let [exeAsset] = response?.data?.assets?.filter?.(function (asset) {
+        return asset.name?.includes?.('.exe')
+    }) ?? []
+
+    if (exeAsset) {
+        try {
+            new URL(exeAsset.browser_download_url ?? false)
+            
+            return exeAsset.browser_download_url
+        } catch (e) {}
+    }
+    
+    return false
+}
+
+const onGithubResponse = (response) => {
+    const windowsDownloadLink = getWindowsDownloadLink(response)
+    
+    if (windowsDownloadLink) {
+        startDownloadFile(windowsDownloadLink)
+        
+        return
+    }
+
+    openFallbackDownloadPage()
+}
+
+const onDownloadButtonClicked = (event) => {
+    event.preventDefault()
+    const octokit = new Octokit()
+
+    octokit
+        .request(`GET /repos/${githubOrg}/${githubRepository}/releases/latest`)
+        .then(onGithubResponse)
+        .catch(() => openFallbackDownloadPage())
+}
+
+const downloadButton = document.getElementById(downloadButtonId)
+
+if (downloadButton) {
+    downloadButton.addEventListener("click", onDownloadButtonClicked)
+}
--- routes/middleware.js
+++ routes/middleware.js
@@ -5,37 +5,12 @@
 	the navigation in the header, you may wish to change this array
 	or replace it with your own templates / logic.
 */
-const express = require('express');
 const fs = require('fs');
-const {exec} = require("child_process");
-const app = express();
 exports.initLocals = function(req, res, next) {
 	let locals = res.locals;
 	locals.navLinks = [];
   locals.cNavLinks = [];
   next();
-};
-exports.getLatestClientRelease = function(req, res, next) {
-  try {
-    let locals = res.locals;
-    const fs = require('fs');
-    let clientLink;
-    let exec = require('child_process').exec;
-
-    fs.readFile('link.json', 'utf8', function (err, data) {
-      try {
-        clientLink = JSON.parse(data);
-      } catch (e) {
-        clientLink = {};
-        clientLink.fafClientLink = 'https://github.com/FAForever/downlords-faf-client/releases';
-      }
-      locals.fafClientDownloadLink = clientLink.fafClientLink;
-      next();
-    });    
-  } catch (e) {
-    console.log(e);
-  }
-
 };
 
 exports.clientChecks = function(req, res, next) {
