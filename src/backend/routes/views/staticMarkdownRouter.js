c7cf211fd34888f0b543cf8d1f343a6664ec53a4
Rümeysa Güven <gguvenrrumeysa@gmail.com>
2025-01-02 15:38:09 +0300

    Lint error fixes

--- src/backend/routes/views/staticMarkdownRouter.js
+++ src/backend/routes/views/staticMarkdownRouter.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 const router = express.Router()
 
 // second argument containerClass can be used to inject a class, for page specific styling
-function markdown(template, containerClass = "") {
+function markdown(template, containerClass = '') {
     return (req, res) => {
         res.render('markdown', {
             content: new showdown.Converter().makeHtml(
                 fs.readFileSync(template, 'utf-8')
             ),
-            containerClass: containerClass
+            containerClass,
         })
     }
 }
@@ -38,6 +38,9 @@ router.get(
 )
 router.get('/rules', markdown('src/backend/templates/views/markdown/rules.md'))
 router.get('/cg', markdown('src/backend/templates/views/markdown/cg.md'))
-router.get('/moderation', markdown('src/backend/templates/views/markdown/moderation.md'))
+router.get(
+    '/moderation', 
+    markdown('src/backend/templates/views/markdown/moderation.md')
+)
 
 module.exports = router
--- src/frontend/js/entrypoint/snowflakes.js
+++ src/frontend/js/entrypoint/snowflakes.js
@@ -9,7 +9,7 @@ script.onload = function () {
     const currentMonth = new Date().getMonth() + 1
     if (
         typeof Snowflakes !== 'undefined' &&
-        (currentMonth <= 2 || currentMonth == 12)
+        (currentMonth <= 2 || currentMonth === 12)
     ) {
         // eslint-disable-next-line no-new, no-undef
         new Snowflakes()
