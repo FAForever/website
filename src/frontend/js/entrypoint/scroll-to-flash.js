bf2a8e7aa99f2c3a975897faf76cf1faa4c362d7
Ivan-Shaml <72102779+Ivan-Shaml@users.noreply.github.com>
2025-01-03 21:36:15 +0200

    make scroll instant

--- src/frontend/js/entrypoint/scroll-to-flash.js
+++ src/frontend/js/entrypoint/scroll-to-flash.js
@@ -3,7 +3,7 @@ const scrollAndHighlight = () => {
     const flashType = element?.getAttribute('data-flash-type')
     if (element && flashType === 'Error!') {
         const originalColor = element.style.backgroundColor
-        element.scrollIntoView({ behavior: 'smooth', block: 'center' })
+        element.scrollIntoView({ behavior: 'instant', block: 'center' })
         element.style.backgroundColor = '#ffc107'
         setTimeout(() => {
             element.style.backgroundColor = originalColor
