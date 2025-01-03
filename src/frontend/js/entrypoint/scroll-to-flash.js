18803425e8910916a8f620526f0f27bc70db2ee4
Ivan-Shaml <72102779+Ivan-Shaml@users.noreply.github.com>
2025-01-03 14:59:01 +0200

    feat(account-linking): better error handling
    * better error handling for Steam and GOG account linking. If error occurs during account linking, now the flash message is being scrolled to in the view port, and it gets highlighted

---
+++ src/frontend/js/entrypoint/scroll-to-flash.js
@@ -0,0 +1,14 @@
+const scrollAndHighlight = () => {
+    const element = document.getElementById('flash-msg-container')
+    const flashType = element?.getAttribute('data-flash-type')
+    if (element && flashType === 'Error!') {
+        const originalColor = element.style.backgroundColor
+        element.scrollIntoView({ behavior: 'smooth', block: 'center' })
+        element.style.backgroundColor = '#ffc107'
+        setTimeout(() => {
+            element.style.backgroundColor = originalColor
+        }, 1700)
+    }
+}
+
+window.onload = () => scrollAndHighlight()
