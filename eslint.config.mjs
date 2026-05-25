adb5320f1f2f03908894a111c6c1b856172a2e0d
Brutus5000 <Brutus5000@gmx.net>
2026-05-25 21:20:19 +0200

    Major version update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,13 @@
+import neostandard from 'neostandard'
+import prettier from 'eslint-plugin-prettier/recommended'
 import { defineConfig } from 'eslint/config'
-import prettier from 'eslint-plugin-prettier'
 import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-})
 
 export default defineConfig([
-    {
-        extends: compat.extends('standard', 'plugin:prettier/recommended'),
-
-        plugins: {
-            prettier,
-        },
-
-        rules: {
-            'prettier/prettier': 'error',
-        },
-    },
+    ...neostandard(),
+    prettier,
     {
         files: ['src/frontend/**/*.js'],
-
         languageOptions: {
             globals: {
                 ...globals.browser,
@@ -37,7 +16,6 @@ export default defineConfig([
     },
     {
         files: ['src/backend/**/*.js'],
-
         languageOptions: {
             globals: {
                 ...globals.node,
@@ -46,7 +24,6 @@ export default defineConfig([
     },
     {
         files: ['tests/**/*.js'],
-
         languageOptions: {
             globals: {
                 ...globals.node,
